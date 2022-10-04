9f6d8992bec7856bd9118bc90b55a3fbf01c9997
Cyanne-Jones <jonescyanne@yahoo.com>
2022-10-04 15:01:21 -0600

    Add basic layout and styling

--- notification-system-frontend/src/App.js
+++ notification-system-frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <nav>
+        <h1>BookFace</h1>
+        <button>
+          Notifications
+        </button>
+      </nav>
+      <main>
+        <p>Your news feed</p>
+      </main>
     </div>
   );
 }
