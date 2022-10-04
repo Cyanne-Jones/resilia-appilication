db5d75c130a0a6e684fac53425b105d83ad84680
Cyanne-Jones <jonescyanne@yahoo.com>
2022-10-04 15:40:04 -0600

    Allow CORS

--- notification-system-api/server.js
+++ notification-system-api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-app.set('port', process.env.PORT || 8000);
+
+app.set('port', 8000);
 
 app.locals.title = 'Notifications';
 
