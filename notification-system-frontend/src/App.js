6ec6daef5d388712476e52c4f89a17e9d2bf8ee3
Cyanne-Jones <jonescyanne@yahoo.com>
2022-10-04 16:09:57 -0600

    Store notifications

--- notification-system-frontend/src/App.js
+++ notification-system-frontend/src/App.js
@@ -1,7 +1,44 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useNotificationStore } from './state/useNotificationStore';
 
 function App() {
+
+  const addNotifications = useNotificationStore(state => state.addNotifications);
+  const [ notifications, setNotifications ] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://localhost:8000/api/v1/notifications")
+      .then(response => {
+        if(response.status === 200) {
+          return response.json()
+        } else {
+          throw new Error("Couldn't fetch data")
+        }
+      })
+      .then(res => {
+        addNotifications(res.notifications);
+        const parsedNotifications = JSON.parse(localStorage.getItem('notification-storage'));
+        setNotifications(parsedNotifications.state.notifications);
+      })
+      .catch(error => console.log(error));
+
+  }, []);
+
+  const mappedNotifications = notifications.map(notification => {
+    return(
+    <div 
+      className="notification" 
+      key={notification.id}
+    >
+      <p>{notification.id}</p>
+    </div>
+    )
+  })
+
+
+
   return (
     <div className="App">
       <nav>
@@ -11,6 +48,7 @@ function App() {
         </button>
       </nav>
       <main>
+        {mappedNotifications}
         <p>Your news feed</p>
       </main>
     </div>
